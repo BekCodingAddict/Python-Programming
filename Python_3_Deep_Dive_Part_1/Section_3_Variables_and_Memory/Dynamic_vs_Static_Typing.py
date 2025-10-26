d814a3ada70a51ddb81275e1d4a54b0f270ebb36
unknown <experte001@gmail.com>
2025-10-27 01:28:32 +0900

    Synamic and Static Typing

---
+++ Python_3_Deep_Dive_Part_1/Section_3_Variables_and_Memory/Dynamic_vs_Static_Typing.py
@@ -0,0 +1,18 @@
+a="hello"
+print(type(a))
+a=10
+print(type(a))
+a=10.5
+print(type(a))
+a=True
+print(type(a))
+a=None
+print(type(a))
+a=10
+print(type(a))
+a=10.5
+print(type(a))
+a=lambda x: x+1
+print(type(a))
+a=3+4j
+print(type(a))
